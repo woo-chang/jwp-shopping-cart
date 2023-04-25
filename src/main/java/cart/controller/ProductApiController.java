eaed4c4087385851c4cd538ab0c9f7c5f5e38919
woo-chang <clllickme@naver.com>
2023-04-26 01:20:57 +0900

    feat: 상품 관리 CRUD API 기능 구현

--- src/main/java/cart/controller/ProductApiController.java
+++ src/main/java/cart/controller/ProductApiController.java
@@ -1,10 +1,14 @@
 package cart.controller;
 
 import cart.dto.request.ProductRequestDto;
+import cart.dto.response.ProductResponseDto;
 import cart.service.ProductService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class ProductApiController {
@@ -15,8 +19,35 @@ public class ProductApiController {
         this.productService = productService;
     }
 
-    @PostMapping(path = "/products")
-    void create(@RequestBody ProductRequestDto productRequestDto) {
-        productService.register(productRequestDto);
+    //CREATE
+    @PostMapping("/products")
+    public ResponseEntity<Void> registerProduct(@Valid @RequestBody ProductRequestDto productRequestDto) {
+        final Long productId = productService.register(productRequestDto);
+
+        return ResponseEntity.created(URI.create("/products/" + productId)).build();
+    }
+
+    //READ
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductResponseDto>> findProducts() {
+        final List<ProductResponseDto> products = productService.findProducts();
+
+        return ResponseEntity.ok(products);
+    }
+
+    //UPDATE
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequestDto productRequestDto) {
+        productService.update(id, productRequestDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    //DELETE
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/cart/dto/request/ProductRequestDto.java
+++ src/main/java/cart/dto/request/ProductRequestDto.java
@@ -1,12 +1,18 @@
 package cart.dto.request;
 
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class ProductRequestDto {
 
+    @Size(min = 1, max = 50, message = "상품명은 {min}보다 크고 {max}보다 짧아야 합니다.")
     private final String name;
+    @Size(min = 1, max = 255, message = "이미지 경로는 {min}보다 크고 {max}보다 짧아야 합니다.")
     private final String imageUrl;
+    @PositiveOrZero(message = "상품 가격은 0보다 작을 수 없습니다.")
     private final Integer price;
+    @Size(min = 1, max = 255, message = "상품 설명은 {min}보다 크고 {max}보다 짧아야 합니다.")
     private final String description;
     private final List<Long> categoryIds;
 
